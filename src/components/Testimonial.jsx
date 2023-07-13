5d5703600e79bd11d93d91c89defc346d6f16a99
@kushagra24 <krishna619kk@gmail.com>
2023-07-13 11:03:27 +0530

    Testimonial section built and made responsive

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import union from "../assets/Union (1).png";
-import avatar from "../assets/avatar1.png";
+import union from "../assets/cloud.png";
+import { testimonial } from "../constants/data";
 
 const Testimonial = () => {
   return (
@@ -8,76 +8,29 @@ const Testimonial = () => {
       <h1 className='text-center font-semibold font-inter text-4xl text-black '>
         What our clients says about us?
       </h1>
-      <div className='flex flex-col lg:flex-row items-center justify-between my-14 gap-10'>
-        <div>
-          <div className='relative'>
-            <img src={union} alt='Image' className='w-full' />
-            <div className='text-white absolute top-20 text-left px-2 lg:top-10  text-[10px] md:text-base min-[375px]:text-xs min-[425px]:text-sm'>
-              <span className='text-left'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </span>
-              <img
-                src={avatar}
-                className='right-0 absolute w-1/3 top-36 lg:top-20'
-              />
-              <p className='float-right font-semibold font-inter mr-24 text-[#FFFFFF] text-base'>
-                Alisa
-              </p>
+      <div className='flex flex-col lg:flex-row items-center justify-between my-14 gap-10 2xl:justify-around'>
+        {testimonial?.map((item, idx) => (
+          <>
+            <div key={idx}>
+              <div className='relative'>
+                <img src={union} alt='Image relative' />
+                <span className='text-base text-left font-medium bottom-16 absolute lg:bottom-12 md:bottom-20 md:text-base lg:text-sm text-white px-8 xl:text-base xl:bottom-20'>
+                  {item.desc}
+                </span>
+                <img
+                  src={item.avatar}
+                  className='right-0 absolute w-1/3 min-[425px]:top-52 md:top-52 lg:top-[185px] xl:top-56 '
+                />
+                <p className='float-right text-[22px] font-semibold bottom-5 right-40 font-inter absolute lg:bottom-2 xl:top-64 xl:right-36 lg:right-[105px] text-white md:right-36 md:bottom-8'>
+                  {item.name}
+                </p>
+              </div>
+              <h1 className='text-xl font-inter text-[#3D7196] font-semibold w-full text-center pr-14 pt-5'>
+                {item.company}
+              </h1>
             </div>
-            <h1 className='text-xl font-inter text-[#3D7196] font-semibold w-full text-center pr-5 pt-5'>
-              Founder,XOXO
-            </h1>
-          </div>
-        </div>
-        <div>
-          <div className='relative'>
-            <img src={union} alt='Image' className='w-full' />
-            <div className='text-white absolute top-20 text-cletext-left px-2 lg:top-10  text-[10px] md:text-base min-[375px]:text-xs min-[425px]:text-sm'>
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </span>
-              <img
-                src={avatar}
-                className='right-0 absolute w-1/3 top-36 lg:top-20'
-              />
-              <p className='float-right font-semibold font-inter mr-24 text-[#FFFFFF] text-base'>
-                Alisa
-              </p>
-            </div>
-            <h1 className='text-xl font-inter text-[#3D7196] font-semibold w-full text-center pr-5 pt-5'>
-              Founder,XOXO
-            </h1>
-          </div>
-        </div>
-        <div>
-          <div className='relative'>
-            <img src={union} alt='Image' className='w-full' />
-            <div className='text-white absolute top-20 text-cletext-left px-2 lg:top-10  text-[10px] md:text-base min-[375px]:text-xs min-[425px]:text-sm'>
-              <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </span>
-              <img
-                src={avatar}
-                className='right-0 absolute w-1/3 top-36 lg:top-20'
-              />
-              <p className='float-right font-semibold font-inter mr-24 text-[#FFFFFF] text-base'>
-                Alisa
-              </p>
-            </div>
-            <h1 className='text-xl font-inter text-[#3D7196] font-semibold w-full text-center pr-5 pt-5'>
-              Founder,XOXO
-            </h1>
-          </div>
-        </div>
+          </>
+        ))}
       </div>
     </div>
   );
--- src/constants/data.js
+++ src/constants/data.js
@@ -6,6 +6,9 @@ import Product from "../assets/product.png";
 import marketing from "../assets/marketing.png";
 import Data from "../assets/DataSciene.png";
 import teaching from "../assets/teaching.png";
+import avatar1 from "../assets/avatar1.png";
+import avatar2 from "../assets/avatar3.png";
+import avatar3 from "../assets/avatar2.png";
 
 export const images = [Amazon, spotify, Fb];
 
@@ -36,4 +39,25 @@ export const serviceTypeOptions = [
   { value: "option1", label: "Option 1" },
   { value: "option2", label: "Option 2" },
   { value: "option3", label: "Option 3" },
+];
+
+export const testimonial = [
+  {
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    name: "Alisa",
+    avatar: avatar1,
+    company: "Founder,XOXO",
+  },
+  {
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    name: "Michel",
+    avatar: avatar2,
+    company: "Manager,QWERTY",
+  },
+  {
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    name: "Lee Cooper",
+    avatar: avatar3,
+    company: "HR Director, Meta",
+  },
 ];
